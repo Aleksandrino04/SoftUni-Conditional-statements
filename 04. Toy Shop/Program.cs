f47409d12889b5a240638f71e1c64e2c15af7784
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-14 23:29:41 +0300

    Add files via upload

---
+++ 04. Toy Shop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            int puzzelQuantity = int.Parse(Console.ReadLine());
+            int dollQuantity = int.Parse(Console.ReadLine());
+            int bearQuantity = int.Parse(Console.ReadLine());
+            int minionQuantity = int.Parse(Console.ReadLine());
+            int truckQuantity = int.Parse(Console.ReadLine());
+
+            int totalQuantity = puzzelQuantity + dollQuantity + bearQuantity + minionQuantity + truckQuantity;
+            double totalPrice = (puzzelQuantity*2.60) + (dollQuantity*3) + (bearQuantity*4.10) + (minionQuantity*8.20) + (truckQuantity*2);
+
+            if (totalQuantity>=100)
+            {
+                totalPrice = totalPrice - (totalPrice * 0.25);
+            }
+            totalPrice = totalPrice - totalPrice * 0.10;
+            if(totalPrice>=tripPrice)
+            {
+                Console.WriteLine($"Yes! {totalPrice-tripPrice:F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {tripPrice-totalPrice:F2} lv needed.");
+            }
+
+
+
+        }
+        
+    }
+}
---
+++ 05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sale = 0;
+            double filmBudget = double.Parse(Console.ReadLine());
+            int extras = int.Parse(Console.ReadLine());
+            double pricePerExtras = double.Parse(Console.ReadLine());
+
+            double decorPrice = filmBudget * 0.10;
+            double totalPriceForExtras = extras * pricePerExtras;
+            if(extras>150)
+            {
+                 sale =  totalPriceForExtras * 0.10;
+            }
+            double totalPrice = totalPriceForExtras-sale + decorPrice;
+            double difference = Math.Abs(totalPrice - filmBudget);
+            if (totalPrice> filmBudget)
+            {
+                Console.WriteLine(	"Not enough money!");
+                Console.WriteLine($"Wingard needs {difference:F2} leva more.");
+
+            }
+           else if (totalPrice<=filmBudget)
+            {
+                Console.WriteLine(	"Action!");
+                Console.WriteLine($"Wingard starts filming with {difference:F2} leva left.");
+
+            }
+
+        }
+    }
+}
